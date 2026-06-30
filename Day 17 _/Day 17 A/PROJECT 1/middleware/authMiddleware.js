e00e2d9e1531f07497bef36d3fb38efb030468ba
unknown <ashayar630@gmail.com>
2026-06-30 19:59:32 +0530

    Day 17_

---
+++ Day 17 _/Day 17 A/PROJECT 1/middleware/authMiddleware.js
@@ -0,0 +1,37 @@
+const jwt = require("jsonwebtoken");
+const User =require("../models/User");
+
+const authMiddleware= async(req,res,next)=>{
+    try{
+        const authHeader = req.headers.authorization;
+
+        if(!authHeader || !authHeader.startsWith("Bearer ")){
+            return res.status(401).json({
+                success:false,
+                message:"Token Not Found"
+            });
+        }
+        //Now work with  token.
+        let token =authHeader.split(" ")[1];
+        const decoded = jwt.verify(token,process.env.SECRET_KEY);
+        const user=await User.findById(decoded.id).select("-password");
+        //without Password
+        if(!user){
+            return res.status(401).json({
+                success:false,
+                message:"User Not Found"
+            });
+        }
+        console.log("Middleware wala user",user);
+        req.user=user;
+        next();
+    }
+    catch(err){
+        console.log("Some Error in authMiddleware",err);
+        return res.json({
+            success:false,
+            message:"Invalid Token"
+        });
+    }
+};
+module.exports=authMiddleware;
